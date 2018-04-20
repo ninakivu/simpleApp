d94a390c57f28ee6f35eec860955718c0d0800a8
Nina Klemencic <nina4klemencic@gmail.com>
2018-04-20 19:39:16 +0200

    basic restful routes

--- models/User.js
+++ models/User.js
@@ -5,6 +5,16 @@ const
     age: Number,
     email: String
   })
-
+  
 const User = mongoose.model('User', userSchema)
 module.exports = User
+
+// creating new user - hardcoded:
+
+// var newUser = new User()
+// newUser.name = "Nina"
+// newUser.age = 25
+// newUser.email = "nina4klemencic@gmail.com"
+// newUser.save(function(err, savedUser){
+//   console.log(savedUser)
+// })
--- server.js
+++ server.js
@@ -2,27 +2,46 @@ const
   express = require('express'),
   app = express(),
   logger = require('morgan'),
+  bodyParser = require('body-parser')
   mongoose = require('mongoose'),
   User = require('./models/User')
 
 
 app.use(logger('dev'))
+app.use(bodyParser.json())
 
 mongoose.connect('mongodb://localhost/simpleappdb'), function(err) {
   console.log(err || "Connected to MongoDB.")
 }
 
 
-app.listen(3000, function(err) {
-  console.log(err || "Server running on 3000.")
-})
+
 
 app.get('/users', function(req, res) {
   User.find({}, function(err, users) {
-    res.send(users)
+    res.json(users)
   })
 })
 
 app.get('/users/:id', function(req, res) {
-  res.send("Profile page for an user: " + req.params.id)
+  User.findById(req.params.id, function(err, thatUser){
+    if(err) {
+      res.json({message:"There was a problem."})
+      console.log(err)
+    } else {
+    res.json(thatUser)
+    }
+  })
+})
+
+// create user:
+app.post('/users', function(req, res) {
+  User.create(req.body, function(err, brandNewUser){
+    if(err) return console.log(err)
+    res.json({message: "User created.", user: brandNewUser})
+  })
+})
+
+app.listen(3000, function(err) {
+  console.log(err || "Server running on 3000.")
 })
